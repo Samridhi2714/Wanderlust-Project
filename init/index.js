f7f31e5940eaa06e8956528797961c8a3f112aca
Samridhi bhatia <samridhibhatia014@gmail.com>
2025-11-28 09:08:55 +0530

    Fixed .env issue, updated folder structure, ready for deployment

--- init/index.js
+++ init/index.js
@@ -1,28 +1,30 @@
 const mongoose = require("mongoose");
 const axios = require("axios");
 const Listing = require("../models/listing.js");
-const User = require("../models/user.js"); 
+const User = require("../models/user.js");
 const initData = require("./data.js");
 
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 const MONGO_URL = process.env.ATLASDB_URL;
 
-main()
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.log("Connection error:", err));
+if (!MONGO_URL) {
+  console.log("ERROR: ATLASDB_URL is missing in .env");
+  process.exit(1);
+}
 
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const seedDB = async () => {
+main()
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("Connection error:", err));
 
-  // STEP 1: delete old data
+const seedDB = async () => {
   await Listing.deleteMany({});
   await User.deleteMany({});
   console.log(" Old listings & users deleted");
 
-  // STEP 2: create demo user
   const demoUser = new User({
     email: "demo@wanderlust.com",
     username: "demoUser",
@@ -31,10 +33,7 @@ const seedDB = async () => {
   const registeredUser = await User.register(demoUser, "demoPassword");
   console.log("Demo user created", registeredUser._id);
 
-  // STEP 3: insert listings
   for (let item of initData.data) {
-
-    // --- geocode ---
     try {
       const response = await axios.get(
         "https://nominatim.openstreetmap.org/search",
@@ -61,14 +60,15 @@ const seedDB = async () => {
       } else {
         item.geometry = { type: "Point", coordinates: [0, 0] };
       }
-    } catch (err) {
+    } catch {
       item.geometry = { type: "Point", coordinates: [0, 0] };
     }
+
     item.owner = registeredUser._id;
 
     const listing = new Listing(item);
     await listing.save();
-    console.log(" Added:", item.title);
+    console.log("Added:", item.title);
   }
 
   mongoose.connection.close();
